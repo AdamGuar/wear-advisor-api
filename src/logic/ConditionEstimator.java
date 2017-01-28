f3358200b3d1de662d0421bb748de9d1345cea52
Adam Wozniak <adam2hp@gmail.com>
2017-01-28 18:22:55 +0100

    Renamed realTemperature to windChill fixed windChill calculation bug

--- src/logic/ConditionEstimator.java
+++ src/logic/ConditionEstimator.java
@@ -5,35 +5,38 @@ import model.Conditions;
 
 public class ConditionEstimator {
 	
-	public static final int FREEZING_TEMP = -10;
-	public static final int COLD_TEMP = -5;
+	public static final int FREEZING_TEMP = -5;
+	public static final int COLD_TEMP = 0;
 	public static final int MODERATE_TEMP = 10;
 	public static final int WARM_TEMP = 25;
 	
 	
 
-	private static double realTemperatureCalculator(double temperature, double windSpeed){		
+	private static double windChillCalculator(double temperature, double windSpeed){		
 		
-		double realTemperature = 13.12 + 0.6215*temperature - 11.37*Math.pow(windSpeed, 0.16)+0.3965*temperature*Math.pow(windSpeed,0.16);
+		double windChill = 13.12 + 0.6215*temperature - 11.37*Math.pow(windSpeed, 0.16)+0.3965*temperature*Math.pow(windSpeed,0.16);
+		if(windChill>temperature) windChill = temperature;
+		else
+			windChill = (double)Math.round(windChill * 10) / 10 ;
 		
-		return realTemperature;
+		return windChill;
 		
 	}
 	
 	
 	public static void estimateConditionsType(Conditions conditions){
 		
-		double realTemperature = realTemperatureCalculator(conditions.getTemperature(),conditions.getWind());
+		double windChill = windChillCalculator(conditions.getTemperature(),conditions.getWind());
 		
-		conditions.setRealTemperature(realTemperature);
+		conditions.setWindChill(windChill);
 		
-		if(realTemperature<FREEZING_TEMP)
+		if(windChill<FREEZING_TEMP)
 			conditions.setType(ConditionType.freezing);
-		else if(realTemperature<COLD_TEMP)
+		else if(windChill<COLD_TEMP)
 			conditions.setType(ConditionType.cold);
-		else if(realTemperature<MODERATE_TEMP)
+		else if(windChill<MODERATE_TEMP)
 			conditions.setType(ConditionType.moderate);
-		else if(realTemperature<WARM_TEMP)
+		else if(windChill<WARM_TEMP)
 			conditions.setType(ConditionType.warm);
 		else
 			conditions.setType(ConditionType.hot);
--- src/model/Conditions.java
+++ src/model/Conditions.java
@@ -4,47 +4,42 @@ public class Conditions {
 
 	double temperature;
 	double wind;
-	double realTemperature;
+	double windChill;
 	ConditionType type;
 	
-
+	
 	public double getTemperature() {
 		return temperature;
 	}
-
 	public void setTemperature(double temperature) {
 		this.temperature = temperature;
 	}
-
 	public double getWind() {
 		return wind;
 	}
-
 	public void setWind(double wind) {
 		this.wind = wind;
 	}
-
+	public double getWindChill() {
+		return windChill;
+	}
+	public void setWindChill(double windChill) {
+		this.windChill = windChill;
+	}
 	public ConditionType getType() {
 		return type;
 	}
-
 	public void setType(ConditionType type) {
 		this.type = type;
 	}
-
-	public double getRealTemperature() {
-		return realTemperature;
-	}
-
-	public void setRealTemperature(double realTemperature) {
-		this.realTemperature = realTemperature;
-	}
-
 	@Override
 	public String toString() {
-		return "Conditions [temperature=" + temperature + ", wind=" + wind + ", realTemperature=" + realTemperature
-				+ ", type=" + type + "]";
+		return "Conditions [temperature=" + temperature + ", wind=" + wind + ", windChill=" + windChill + ", type="
+				+ type + "]";
 	}
+	
+
+
 	
 	
 	
